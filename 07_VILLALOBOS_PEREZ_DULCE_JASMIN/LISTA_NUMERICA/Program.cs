34b32ff1cdc52276b6d84eb7d8ba9981596996d5
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2018-09-23 20:55:24 -0700

    Initial commit

---
+++ 07_VILLALOBOS_PEREZ_DULCE_JASMIN/LISTA_NUMERICA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_NUMERICA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random desorden = new Random();
+            List<int> numeros = new List<int>(); string si;
+            Console.Write("\n\t\bSUCESION NUMERICA\n");
+            do { try {                  
+                    for (int contar = 1; contar <=100; contar++)
+                    {
+                      
+                        Console.Write(" {0} ", +contar); // numeros.Add(contar);  
+                        numeros.Add(desorden.Next(1, 100));
+                    }                    
+                    Console.Write("\n"); numeros.Reverse();
+                    foreach (int suscecion in numeros)
+                    {
+                        Console.Write("{0}", suscecion); Console.Write("||");                       
+                    }   }
+                catch (Exception) { Console.Write("\n\t\aERROR"); }
+                Console.Write("\n\n\tNUMEROS : {0}", numeros.LongCount());              
+                Console.Write("\n\t\tREINIZAR (SI || NO) : "); si = Console.ReadLine();
+                numeros.Clear();
+            }  while (si == "si" || si == "SI");  Console.ReadKey();
+        }
+    }
+}
---
+++ 08_VILLALOBOS_PEREZ_DULCE_JASMIN/PALINDROMO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PALINDROMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  string si; int numeroPalindromo = 0;
+            do
+            {
+                Console.Write("\n\tPALINDROMO\n");
+                try
+                {
+                    Console.Write("\n\tINGRESE 5 DIGITOS : ");
+                    numeroPalindromo = int.Parse(Console.ReadLine());
+
+                    if (numeroPalindromo >= 10000 && numeroPalindromo <= 99999)
+                    {
+                        char[] reves = numeroPalindromo.ToString().ToCharArray();
+                        int palindromoReves =int.Parse(Convert.ToString(reves[4]) +
+                            Convert.ToString(reves[3]) + Convert.ToString(reves[2]) +
+                            Convert.ToString(reves[1]) + Convert.ToString(reves[0]));
+                        if (palindromoReves == numeroPalindromo)
+                            Console.Write("\n\t{0} IGUAL {1} PALINDROMO\n", numeroPalindromo, palindromoReves);
+                        else
+                            Console.Write("\n\t{0} NO ES IGUAL {1} NO PALINDROMO\n", numeroPalindromo, palindromoReves);
+                    }
+                    else Console.Write("\t\aERRRO\n");
+                }
+                catch (FormatException) { Console.Write("\n\t\aLA CADENA DE DATOS NO TIENE EL FORMATO ASIGNADO\n"); }
+                catch(OverflowException) { Console.Write("\n\t\aLA CADENA DE DATOS ESTA FUERA DEL RANGO ASIGNADO\n"); }
+                finally { Console.Write("\n\t\tREINIZAR (SI || NO) : "); si = Console.ReadLine(); }
+            }
+            while (si == "si" || si == "SI"); Console.ReadKey();
+        }
+    }
+}
